84595772d023d046af3584f432d8bef0d3735509
BANI GIT <bani.rezaie@gmail.com>
2021-06-19 05:26:31 +0100

    initial data

--- mockData.js
+++ mockData.js
@@ -1,80 +1,80 @@
 const courses = [
   {
     id: 1,
-    title: "Securing React Apps with Auth0",
-    slug: "react-auth0-authentication-security",
+    title: "IMD class 1 html course",
+    slug: "html",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 2,
-    title: "React: The Big Picture",
-    slug: "react-big-picture",
+    title: "IMD class 1 css course",
+    slug: "css",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 3,
-    title: "Creating Reusable React Components",
-    slug: "react-creating-reusable-components",
+    title: "IMD class 1 bootstrap course",
+    slug: "bootstrap",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 4,
-    title: "Building a JavaScript Development Environment",
-    slug: "javascript-development-environment",
+    title: "IMD class 1 vanilla js module 1 course",
+    slug: "imd-1-js-1",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 5,
-    title: "Building Applications with React and Redux",
-    slug: "react-redux-react-router-es6",
+    title: "IMD class 1 vanilla js module 2 course",
+    slug: "imd-1-js-2",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 6,
-    title: "Building Applications in React and Flux",
-    slug: "react-flux-building-applications",
+    title: "IMD class 1 vanilla js module 3 course",
+    slug: "imd-1-js-3",
     authorId: 1,
     category: "JavaScript"
   },
   {
     id: 7,
-    title: "Clean Code: Writing Code for Humans",
-    slug: "writing-clean-code-humans",
+    title: "IMD class 1 Node js course",
+    slug: "node-js",
     authorId: 1,
     category: "Software Practices"
   },
   {
     id: 8,
-    title: "Architecting Applications for the Real World",
-    slug: "architecting-applications-dotnet",
+    title: "IMD class 1 express js course",
+    slug: "express-js",
     authorId: 1,
     category: "Software Architecture"
   },
   {
     id: 9,
-    title: "Becoming an Outlier: Reprogramming the Developer Mind",
-    slug: "career-reboot-for-developer-mind",
+    title: "IMD class 1 Mongo DB course",
+    slug: "mongo-db",
     authorId: 1,
     category: "Career"
   },
   {
     id: 10,
-    title: "Web Component Fundamentals",
-    slug: "web-components-shadow-dom",
+    title: "IMD class 1 Mongoose course",
+    slug: "mongoose",
     authorId: 1,
     category: "HTML5"
   }
 ];
 
 const authors = [
-  { id: 1, name: "Cory House" },
-  { id: 2, name: "Scott Allen" },
-  { id: 3, name: "Dan Wahlin" }
+  { id: 1, name: "Buffon" },
+  { id: 2, name: "Bartez" },
+  { id: 3, name: "David Siman" }
 ];
 
 const newCourse = {
